381cf34cd002c8d5d972ea08360149c26c0bc02b
Aijana2008 <Aizhanse.90@gmail.com>
2020-09-16 18:41:15 -0400

    Office hours

---
+++ src/warmup/Abstract/Shape.java
@@ -0,0 +1,20 @@
+package warmup.Abstract;
+/*
+1. create an abstract class called Shape
+        attributes:
+            name(static), area, perimeter, hasVolume(static), volume
+        abstract methods: calculateArea(), calculatePerimeter, calculateVolume()
+                            return-types: double
+
+ */
+public class Shape {
+
+    public static String name;
+    public double area;
+    public double perimeter;
+    public boolean hasVolume;
+    public double volume;
+
+
+
+}
