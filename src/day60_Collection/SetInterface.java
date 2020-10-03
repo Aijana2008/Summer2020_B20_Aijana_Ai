5bc00dc10e7ecbd0fa2f806608d429c4bc0fcf9f
Aijana2008 <Aizhanse.90@gmail.com>
2020-10-03 17:15:00 -0400

    day60 class

---
+++ src/day60_Collection/SetInterface.java
@@ -0,0 +1,20 @@
+package day60_Collection;
+
+import java.util.*;
+
+public class SetInterface {
+
+    public static void main(String[] args) {
+
+        Set<Integer> s1 = new HashSet<>();
+        s1.addAll(Arrays.asList(8,5,4,3,2,10,71)); // does not keep the insertion order
+        Set<Integer> s2 = new LinkedHashSet<>();
+
+        Set<Integer> s3 = new TreeSet<>();
+
+
+
+
+
+    }
+}
